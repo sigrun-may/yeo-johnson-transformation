3e008512dcb7933b01c1486ec3dfaaafd874f092
Sigrun May <sigrun.may@gmail.com>
2023-03-03 09:34:40 +0100

    Integrate exception handling. Add automated parameter search.

---
+++ python_examples/example_automated_transformation.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from python_bindings.c_accesspoint import automated_yeo_johnson_power_transformation
+
+# adapt data path and parsing
+dataFrame = pd.read_csv("../x64/data/generated_test.csv", header=0, low_memory=False)
+print(dataFrame.shape)
+my_unlabeled_data_np = dataFrame.to_numpy()
+
+# for library compilation see README.md
+path_to_c_library = "../x64/bin/comInterface.so"
+
+data, lambdas, skews, error_codes = automated_yeo_johnson_power_transformation(
+    path_to_c_library=path_to_c_library,
+    unlabeled_data_np=my_unlabeled_data_np,  # 2D numpy array expected
+)
+print(max(lambdas))
+print(min(lambdas))
+print("Exit status: ", max(error_codes))
---
+++ python_examples/example_yeo_johnson_transformation.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from python_bindings.c_accesspoint import yeo_johnson_power_transformation
+
+# adapt data path and parsing
+dataFrame = pd.read_csv("../x64/data/generated_test.csv", header=0, low_memory=False)
+print(dataFrame.shape)
+my_unlabeled_data_np = dataFrame.to_numpy()
+
+# for library compilation see README.md
+path_to_c_library = "../x64/bin/comInterface.so"
+
+data, lambdas, skews, error_codes = yeo_johnson_power_transformation(
+    path_to_c_library=path_to_c_library,
+    unlabeled_data_np=my_unlabeled_data_np,  # 2D numpy array expected
+    interval_start=-3,  # lower bound for lambda
+    interval_end=3,  # upper bound for lambda
+    interval_parameter=14,  # precision
+    standardize=True,
+    time_stamps=False,
+    number_of_threads=1,
+)
+print(max(lambdas))
+print(min(lambdas))
+print("Exit status: ", max(error_codes))
